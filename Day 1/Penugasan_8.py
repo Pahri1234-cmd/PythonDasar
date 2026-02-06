ec836c390f86e2c7a48f6d9adfabf176ea4162e0
Pahri1234-cmd <m.fahri2292.sma1@sma.prestasiprima.sch.id>
2026-02-06 08:23:44 +0700

    Add files via upload

---
+++ Day 1/Penugasan_8.py
@@ -0,0 +1,14 @@
+# Program Klasifikasi Umur
+
+umur = int(input("Masukkan umur: "))
+
+if umur <= 10:
+    print("Kategori: Anak-anak")
+elif umur <= 18:
+    print("Kategori: Remaja")
+elif umur <= 35:
+    print("Kategori: Dewasa")
+elif umur <= 65:
+    print("Kategori: Parubaya")
+else:
+    print("Kategori: Tua")
